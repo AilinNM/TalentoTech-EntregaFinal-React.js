1647099e8b1ee3887657a4af7b74897ed4c14884
Ailin <61848026+AilinNM@users.noreply.github.com>
2025-12-16 00:40:22 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Quitamos BrowserRouter/Router de aquí
 import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer } from 'react-toastify';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -20,7 +19,7 @@ import Inicio from "./pages/Inicio";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 import ProductoDetalle from "./pages/ProductoDetalle";
-//import Carrito from "./pages/Carrito";
+import Carrito from "./components/Carrito";
 import Nosotros from "./pages/Nosotros";
 import ResultadosBusqueda from "./components/ResultadosBusqueda";
 import RutaProtegida from "./components/RutaProtegida";
@@ -32,52 +31,52 @@ function App() {
         <ProductosProvider>
           <CarritoProvider>
             <SearchProvider>
-              <Router>
-                <div className="app-container d-flex flex-column min-vh-100">
-                  <SEO />
-                  
-                  <Header />
-                  
-                  <main className="flex-grow-1">
-                    <Routes>
-                      {/* Rutas públicas */}
-                      <Route path="/" element={<Inicio />} />
-                      <Route path="/login" element={<Login />} />
-                      <Route path="/nosotros" element={<Nosotros />} />
-                      <Route path="/productos/:id" element={<ProductoDetalle />} />
-                      <Route path="/busqueda" element={<ResultadosBusqueda />} />
-                      
-                      {/* Rutas protegidas */}
-                      <Route path="/carrito" element={
-                        <RutaProtegida>
-                          <Carrito />
-                        </RutaProtegida>
-                      } />
-                      
-                      <Route path="/admin" element={
-                        <RutaProtegida requireAdmin={true}>
-                          <Admin />
-                        </RutaProtegida>
-                      } />
-                    </Routes>
-                  </main>
-                  
-                  <Footer />
-                  
-                  <ToastContainer 
-                    position="top-right"
-                    autoClose={3000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                  />
-                </div>
-              </Router>
+              {/* SE ELIMINÓ: <Router> */}
+              <div className="app-container d-flex flex-column min-vh-100">
+                <SEO />
+                
+                <Header />
+                
+                <main className="flex-grow-1">
+                  <Routes>
+                    {/* Rutas públicas */}
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/nosotros" element={<Nosotros />} />
+                    <Route path="/productos/:id" element={<ProductoDetalle />} />
+                    <Route path="/busqueda" element={<ResultadosBusqueda />} />
+                    
+                    {/* Rutas protegidas */}
+                    <Route path="/carrito" element={
+                      <RutaProtegida>
+                        <Carrito />
+                      </RutaProtegida>
+                    } />
+                    
+                    <Route path="/admin" element={
+                      <RutaProtegida requireAdmin={true}>
+                        <Admin />
+                      </RutaProtegida>
+                    } />
+                  </Routes>
+                </main>
+                
+                <Footer />
+                
+                <ToastContainer 
+                  position="top-right"
+                  autoClose={3000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="light"
+                />
+              </div>
+              {/* SE ELIMINÓ: </Router> */}
             </SearchProvider>
           </CarritoProvider>
         </ProductosProvider>
